5ba4084d6612d7a605aa0ba68deeb7ea17930d62
Olivier St-Laurent <kzaurckichz@gmail.com>
2021-04-08 11:07:00 -0400

    refactored glm config as part of inconsistency fix

--- xvk.hpp
+++ xvk.hpp
@@ -17,9 +17,6 @@
 // GLM
 #ifdef XVK_INCLUDE_GLM
 	// GLM
-	#define GLM_FORCE_RADIANS
-	#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-	#define GLM_ENABLE_EXPERIMENTAL
 	#include "glm/glm/glm.hpp"
 	#include "glm/glm/gtc/matrix_transform.hpp"
 	#include "glm/glm/gtx/matrix_interpolation.hpp"
